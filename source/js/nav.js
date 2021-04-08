60fcc460db41e86e280b53070b641e5d0ba4062f
Антон Корельский <korantonkor@list.ru>
2021-04-08 16:02:11 +0300

    Начало оформления планшетной версии главной страницы

--- source/js/nav.js
+++ source/js/nav.js
@@ -3,7 +3,8 @@ const main_nav = document.querySelector(".main-nav");
 console.log(burger);
 console.log(main_nav);
 
-main_nav.classList.remove("main-nav--opened");
+main_nav.classList.remove("main-nav--nojs");
+burger.classList.remove("page-header__burger--nojs");
 main_nav.classList.add("main-nav--closed");
 
 burger.onclick = function() {
