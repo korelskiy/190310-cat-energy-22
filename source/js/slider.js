f3c90562f78688014d8b229f4ce399d4f04e6950
Антон Корельский <korantonkor@list.ru>
2021-05-16 00:10:54 +0300

    Слайдер 3

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,15 +1,15 @@
 const btn_before = document.querySelector(".effect-slider__label--before");
 const btn_after = document.querySelector(".effect-slider__label--after");
-const img_after = document.querySelector(".effect-slider__item--before");
-const img_before = document.querySelector(".effect-slider__item--after");
+const img_after = document.querySelector(".effect-slider__image--before");
+const img_before = document.querySelector(".effect-slider__image--after");
 const range = document.querySelector(".effect-slider__range");
 
 btn_before.onclick = function() {
-  img_before.classList.remove("effect-slider__item--off");
-  img_after.classList.add("effect-slider__item--off");
+  img_before.classList.remove("effect-slider__image--off");
+  img_after.classList.add("effect-slider__image--off");
 }
 
 btn_after.onclick = function() {
-  img_after.classList.remove("effect-slider__item--off");
-  img_before.classList.add("effect-slider__item--off");
+  img_after.classList.remove("effect-slider__image--off");
+  img_before.classList.add("effect-slider__image--off");
 }
